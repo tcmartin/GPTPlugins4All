917c325ca47aee3d4094b64585eaeaa1c9ce1e97
Trevor <seekerman28@gmail.com>
2023-11-19 00:51:52 -0600

    stuff

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gpt-plugins-4all',
+    name='GPTPlugins4All',
     version='1.0.0',
     packages=find_packages(),
     description='GPT Plugins for 4all',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author_email='trevor@zenithai.io',
+    url='https://github.com/tcmartin/gpt-plugins-4all',
     install_requires=[
-        uuid, json, yaml, requests, openapi_v3_spec_validator, urlencode
+        'uuid', 'json', 'yaml', 'requests', 'openapi_v3_spec_validator', 'urlencode'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
