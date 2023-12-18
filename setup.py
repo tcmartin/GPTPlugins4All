ec247cd410de94c932a8aef054a62e78b5587c4d
Trevor <seekerman28@gmail.com>
2023-12-17 21:54:34 -0600

    gitignore

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='GPTPlugins4All',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(),
     description='GPT Plugins for 4all',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author_email='trevor@zenithai.io',
-    url='https://github.com/tcmartin/GPTPlugins4All',
+    author_email='trevor@gptplugins4all.com',
+    url='https://github.com/DataStreams-Solutions/GPTPlugins4All',
     project_urls={
-        "Bug Tracker": "https://github.com/tcmartin/GPTPlugins4All/issues",
-        "Documentation": "https://github.com/tcmartin/GPTPlugins4All#readme",
-        "Source Code": "https://github.com/tcmartin/GPTPlugins4All",
+        "Bug Tracker": "https://github.com/DataStreams-Solutions/GPTPlugins4All/issues",
+        "Documentation": "https://github.com/DataStreams-Solutions/GPTPlugins4All#readme",
+        "Source Code": "https://github.com/DataStreams-Solutions/GPTPlugins4All",
     },
     install_requires=[
         'PyYAML', 'requests', 'openapi-spec-validator'
@@ -27,7 +27,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'gpt-plugins-4all=GPTPlugins4All.cli:main',  # "gpt-plugins-4all" is the command users will type
+            'gpt-plugins-4all=GPTPlugins4All.cli:main',
         ],
     },
 )
